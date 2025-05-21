30ebd12fba6b720602e45a4cea8c1bed83bb31ba
Brian Richu <richubrian427@gmail.com>
2025-05-21 14:03:41 +0300

    add basic operations python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+addition_result = number1 + number2
+print("Addition of 10 and 5 is ", addition_result)
+
+subtraction_result = number1 - number2
+print("Subtraction of 10 and 5 is ", subtraction_result)
+
+multiplication_result = number1 * number2
+print("Multiplication of 10 and 5 is ", multiplication_result)
+
